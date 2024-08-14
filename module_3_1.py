3b6c1b3a8771340c2b578ec89218ebf27163010e
Sergi0iv <rickitir2.0@gmail.com>
2024-08-14 19:09:10 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(a):
+    count_calls()
+    return len(a), a.upper(), a.lower()
+
+
+def is_contains(a, e):
+    count_calls()
+    for i in e:
+        if a == i:
+            return True
+    return False
+
+
+print(string_info('sergey'))
+print(string_info('kozmodemyansk'))
+print(is_contains('Sergey', ['Andrey', 'Sergey', 'Anna']))
+print(is_contains('Nikolay', ['Sergey', 'Andrey', 'Anna']))
+print(calls)
